e320e401bef8d28a058c080d5c79ec75d44d97e1
Edward <edward@correctsoftware.co>
2021-07-05 21:01:27 +0100

    Simple parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,34 @@
-module Main where
+ module Main where
+
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces :: skipMany1 space
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value"
+
+parseString :: Parser LispVal
+parseString = do
+                char '"'
+                x <- many (noneOf "\"")
+                char '"'
+                return $ String x
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do 
+         (expr:_) <- getArgs
+         putStrLn (readExpr expr)
